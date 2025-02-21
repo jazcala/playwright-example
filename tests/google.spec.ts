e76874830c85a544170ae506052a607fc9f2a8a6
jazcala <jazcalabrese@gmail.com>
2025-02-21 14:34:57 -0300

    Updating tests

--- tests/google.spec.ts
+++ tests/google.spec.ts
@@ -4,7 +4,7 @@ test.describe('Google', () => {
 
   test('has title', async ({ page }) => {
     await page.goto('https://www.google.com')
-    await expect(page).toHaveTitle(/Gogle/)
+    await expect(page).toHaveTitle(/G0ogle/)
   })
 
   test('has button', async ({ page }) => {
